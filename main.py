ec6b2bceda51cf57ad5c975b0e368bd00a92b34e
Simon Brandwood <simon@plet.co.uk>
2023-02-26 12:30:39 +0000

    Extended to add 3 neopixels

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random,  time
 Button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
 Button2 = machine.Pin(1, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-NeoP = neopixel.NeoPixel(machine.Pin(3), 2)
+NeoP = neopixel.NeoPixel(machine.Pin(3), 3)
 
 
 
@@ -12,6 +12,7 @@ mx = 3
 while True:
     NeoP[0] = (random.randint(0, mx), random.randint(0, mx), random.randint(0, mx))
     NeoP[1] = (random.randint(0, mx), random.randint(0, mx), random.randint(0, mx))
+    NeoP[2] = (random.randint(0, mx), random.randint(0, mx), random.randint(0, mx))
     NeoP.write()
     
     #Note, this is a terrible way of getting button input as it relys on the state of the
