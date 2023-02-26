ca192b63d126a3b788a7e63a0b2f60585185791f
Simon Brandwood <simon@plet.co.uk>
2023-02-26 12:27:20 +0000

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pico_neokey_example
 Example of using neokeys with micropython
+
+Buttons and neopixels are separate entities:
+
+- Buttons: Just a simple circuit (2 wires). Each button needs it's own GPIO connection.
+
+ Connect A to 3v3(out) and C to assigned GPIO. Either side of circuitboard is OK
+
+
+- Neopixels:
+
+  Power: + to 3v3(out) and - to GND (seemingly controlling white but not actually needed to get something out - there will be light with + only!)
+
+  GPIO: neopixels work in serial and only 1 GPIO pin is required. Connect GPIO to I of first neopixel and O to I of next neopixel. repeat O to I for all neopixels. No O connection required for last neopixel.
---
+++ main.py
@@ -0,0 +1,24 @@
+import machine, neopixel
+import random,  time
+
+Button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
+Button2 = machine.Pin(1, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+NeoP = neopixel.NeoPixel(machine.Pin(3), 2)
+
+
+
+mx = 3
+while True:
+    NeoP[0] = (random.randint(0, mx), random.randint(0, mx), random.randint(0, mx))
+    NeoP[1] = (random.randint(0, mx), random.randint(0, mx), random.randint(0, mx))
+    NeoP.write()
+    
+    #Note, this is a terrible way of getting button input as it relys on the state of the
+    # loop at the time of the button press. It is only useful as a way to prove the buttons
+    # are connected properly
+    if Button.value():
+        print("Button 1 working!")
+    elif Button2.value():
+        print("Button 2 working!")
+    time.sleep(0.4)
